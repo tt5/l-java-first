2249c7b5c7759c39780c90ceb02544d905719bbd
n <2585803+tt5@users.noreply.github.com>
2024-12-08 15:59:24 +0100

    anonymous main function*

--- src/main/java/first/App.java
+++ src/main/java/first/App.java
@@ -4,7 +4,8 @@ package first;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public void main() {
+    System.out.print("hello anonymous main class");
+  }
+
 }
